22e8e2bff14f0a7ad885d48a5f773c47f891e925
rafaelpoluha <163421927+rafaelpoluha@users.noreply.github.com>
2025-03-10 09:22:08 -0300

    diminuir botão

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 document.addEventListener('DOMContentLoaded', function(){
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuaFonteBotao = document.getElementById('aumentar-fonte');
     let tambemAumentarFonte  = 1;
     aumentaFonteBotao.addEventListener('click',function( ){
         tambemAumentarFonte += 0.1;
-        document.body.style.fontSize = ${tamanhoAtualFonte}
+        document.body.style.fontSize = ${tamanhoAtualFonte}ren';
+    });
+
+    diminuaFonteBotao.addEventListener('click',function( ){
+        tambemdiminuaFonte -= 0.1;
+        document.body.style.fontSize = ${tamanhoAtualFonte}ren';
     });
 });
