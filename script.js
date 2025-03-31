7b10e70b6d7e64f2b6d1a87c9bf2aac47f866a6a
rafaelpoluha <163421927+rafaelpoluha@users.noreply.github.com>
2025-03-31 11:09:11 -0300

    acessibilidade

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 document.addEventListener('DOMContentLoaded', function(){
+   const botaoacessibilidade = document.getElementById ('botão - acessibilidade');
+   const opcoesdeacessibilidade = document.getElementById ('opcoes-acessibiladade');
+
+   botaoacessibilidade.addEventListener ('click', funcao(){
+   botaoacessibilidade.classlist.toggle('rotacao-botao');
+   botaoacessibilidade.classlist.toggle ('apresenta-lista');
+   })
+
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuaFonteBotao = document.getElementById('aumentar-fonte');
+
     let tambemAumentarFonte  = 1;
     aumentaFonteBotao.addEventListener('click',function( ){
         tambemAumentarFonte += 0.1;
